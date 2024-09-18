c0b8d1cbb0e8b8ac93f4823bc83739270038c055
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-09-18 01:33:53 -0300

    feat: Add HTTP server file

---
+++ src/http/server.ts
@@ -0,0 +1,10 @@
+import Elysia from 'elysia';
+
+const app = new Elysia()
+  .get('/', () => {
+    return 'Hello, World!';
+  });
+
+app.listen(3333, () => {
+  console.log('🔥 Server is running on port 3333');
+})
