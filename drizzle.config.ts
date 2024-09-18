0fab7145a75fc894de9a51f62be6ae1c5ff9f162
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-09-18 01:33:15 -0300

    feat: Add drizzle.config.ts for PostgreSQL database configuration

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'drizzle-kit';
+import { env } from './src/env';
+
+
+export default defineConfig({
+  schema: "./src/db/schema/index.ts",
+  out: "./.migrations",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  dialect: 'postgresql'
+})
